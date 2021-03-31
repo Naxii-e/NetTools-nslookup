7e907d63314da5ef9c20c1ccc69e747cd0921b56
Naxii <78034307+Naxii-e@users.noreply.github.com>
2021-03-31 15:21:15 +0900

    first

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# net-tools
+# NetTools-nslookup
+
+## About
+The front-end is developed using Bootstrap, and the back-end is developed using Python3 with Flask and jinja2.
+The `socket` library is used to retrieve the results.
+
+## Structure
+
+main_example.py (逆引きの例)
+```python
+import socket
+
+def reverse_lookup(domain):
+    try:
+        return socket.gethostbyname(domain)
+    except:
+        return "No result"
+
+result = reverse_lookup("example.com")
+print(result) #203.0.113.0
+```
+
+`socket.gethostbyaddr()[0]`で正引きの結果を取得します。逆に`socket.gethostbyname()`で逆引きの結果を取得します。
+上記のコードは逆引きの例です。
+`main.py`では、POSTされたドメイン名(FQDN)又はIPv4アドレスを正引き・逆引きして返すシンプルな構造です。
+なお、IPv6には対応していません。
+
+## Libraries
+- Flask
+- jinja2
+- request
+- jsonify
+- socket
+Other dependent libraries are required.
+
+# License
+The source code is licensed MIT. The website content is licensed CC BY 4.0,see LICENSE.
---
+++ main.py
@@ -0,0 +1,58 @@
+
+#        Copyright (c) 2021 Naxii
+#        This code is released under the MIT License, see LICENSE, see LICENSE.
+#        This website content is released under the CC BY 4.0 License, see LICENSE.
+
+from flask import Flask, jsonify, render_template, request
+import socket, secret
+#secretはIPアドレスとドメインのブラックリストを`blacklist`という変数で保存しています。
+
+app = Flask(__name__)
+
+def forward_lookup(ip):
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return "結果なし"
+def reverse_lookup(domain):
+    try:
+        return socket.gethostbyname(domain)
+    except:
+        return "結果なし"
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="nslookup")
+
+@app.route("/forward", methods=["GET", "POST"])
+def forward():
+    ipadd = request.form.get("ipadd")
+    result = ""
+    result = str(forward_lookup(ipadd))
+    print(result in secret.blacklist)
+    if ((result in secret.blacklist) or (ipadd in secret.blacklist)) == True:
+        #念には念を入れてということで二重検証(実際POSTされたやつだけ検証しとけばいいけどこっちのほうが確証がありそうなので)
+        result = "結果なし"
+    return render_template("forward.html", result=result, title="nslookup - 正引き(forward)")
+
+@app.route("/reverse", methods=["GET", "POST"])
+def reverse():
+    domain = request.form.get("domain")
+    result = str(reverse_lookup(domain))
+    if ((result in secret.blacklist) or (domain in secret.blacklist)) == True:
+        result = "結果なし"
+    return render_template("reverse.html", result=result, title="nslookup - 逆引き(reverse)")
+
+@app.errorhandler(500)
+def server_error(error):
+    return jsonify({
+        'error':'internal error'
+    }), 500
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'error':'not found'
+    }), 404
+
+app.run(debug=False)
